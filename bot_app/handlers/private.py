75ac281d0d2fff54b86f2f13f21543d5258613b2
Emir Magalov <emir.mahalo@gmail.com>
2026-06-30 01:34:27 +0400

    fix

--- bot_app/handlers/private.py
+++ bot_app/handlers/private.py
@@ -6,9 +6,11 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types.web_app_info import WebAppInfo
 from bot_app.config import settings
 from aiogram.types import FSInputFile
+
 url = settings.url
 private_router = Router()
 START_PHOTO_ID = None
+
 
 def web_kb(data: dict[str, str]):
     builder = InlineKeyboardBuilder()
@@ -21,14 +23,13 @@ def web_kb(data: dict[str, str]):
     return builder.adjust(1).as_markup()
 
 
-text = """What's inside our mini app?
+text = """✨ Total portfolio control: track your bags, asset spread, and tx history in real-time.
 
-✨ Total portfolio control: track your bags, asset spread, and tx history in real-time.
 📊 Market analytics: monitor crypto prices, charts, and trends straight from the wallet ui.
-📝 Built-in tax reports: auto-calculates short and long-term gains for easy tax prep (taxation is theft, but audits are worse).
-⚙️ Deep customization: tweak widgets, pick your fiat, and manage how your data displays.
 
-🔒The kicker: your private keys stay locked on your ledger. we just ported the convenience of the ui to telegram while keeping security at absolute max. 🔒"""
+📝 Built-in tax reports: auto-calculates short and long-term gains for easy tax prep (taxation is theft, but audits are worse).
+
+⚙️ Deep customization: tweak widgets, pick your fiat, and manage how your data displays."""
 
 
 @private_router.message(Command("start"))
@@ -37,13 +38,15 @@ async def cmd_start(message: Message):
 
     if START_PHOTO_ID:
         try:
-            await message.answer_photo(photo=START_PHOTO_ID, caption=text,reply_markup=web_kb({'Open Wallet': 'web', "Ledger | Official News": 'url'}))
+            await message.answer_photo(photo=START_PHOTO_ID, caption=text,
+                                       reply_markup=web_kb({'Open Wallet': 'web', "Ledger | Official News": 'url'}))
             return
         except Exception:
             # Если Telegram вдруг скажет "Invalid file_id", сбросим его
             START_PHOTO_ID = None
 
     photo = FSInputFile("bot_app/assets/main_photo.jpg")
-    msg = await message.answer_photo(photo=photo, caption=text,reply_markup=web_kb({'Open Wallet': 'web', "Ledger | Official News": 'url'}))
+    msg = await message.answer_photo(photo=photo, caption=text,
+                                     reply_markup=web_kb({'Open Wallet': 'web', "Ledger | Official News": 'url'}))
 
     START_PHOTO_ID = msg.photo[-1].file_id
